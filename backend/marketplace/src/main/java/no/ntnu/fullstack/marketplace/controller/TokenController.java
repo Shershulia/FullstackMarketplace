0ea39e8ae845334f6dbd9bdc403ebf6024111da7
Adrian Gunnar Lauterer <adrian@lauterer.it>
2023-03-21 17:38:35 +0100

    still borked

--- backend/marketplace/src/main/java/no/ntnu/fullstack/marketplace/controller/TokenController.java
+++ backend/marketplace/src/main/java/no/ntnu/fullstack/marketplace/controller/TokenController.java
@@ -1,11 +1,19 @@
 package no.ntnu.fullstack.marketplace.controller;
 
 
+import no.ntnu.fullstack.marketplace.model.User;
+import no.ntnu.fullstack.marketplace.model.UserInfoResponse;
+import no.ntnu.fullstack.marketplace.service.UserServiceImpl;
+import no.ntnu.fullstack.marketplace.service.UserService;
+
+//import no.ntnu.fullstack.marketplace.dao.UserDao;
+import no.ntnu.fullstack.marketplace.model.LoginRequest;
+
+
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import no.ntnu.fullstack.marketplace.dao.MockDao;
-import no.ntnu.fullstack.marketplace.model.LoginRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -26,29 +34,22 @@ public class TokenController {
 
     //injected from the environment
     //public static final String keyStr = System.getenv("JWT_SECRET_KEY");
-
-    // t key
     private static final Duration JWT_TOKEN_VALIDITY = Duration.ofSeconds(15); //really short validity for testing purpose
-
-
     @PostMapping(value = "")
     @ResponseStatus(value = HttpStatus.CREATED)
     public String generateToken(final @RequestBody LoginRequest loginRequest) throws Exception {
         // if username and password are valid, issue an access token
-        // note that subsequent requests need this token
-        if (MockDao.checkUserCredentials(loginRequest.username(), loginRequest.password())) {
-            return generateToken(loginRequest.username());
-        }
+        // TODO: check if user exists in database and if password is correct before issuing a token
 
-        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Access denied, wrong credentials....");
+        return generateToken(loginRequest.username());
     }
 
-    public String generateToken(final String userId) {
+    public String generateToken(final String userName) {
         final Instant now = Instant.now();
         final Algorithm hmac512 = Algorithm.HMAC512(keyStr);
         final JWTVerifier verifier = JWT.require(hmac512).build();
         return JWT.create()
-                .withSubject(userId)
+                .withSubject(userName)
                 .withIssuer("fullstack_marketplace_api")
                 .withIssuedAt(now)
                 .withExpiresAt(now.plusMillis(JWT_TOKEN_VALIDITY.toMillis()))
--- backend/marketplace/src/main/java/no/ntnu/fullstack/marketplace/controller/UserInfoController.java
+++ backend/marketplace/src/main/java/no/ntnu/fullstack/marketplace/controller/UserInfoController.java
@@ -1,19 +1,19 @@
-package no.ntnu.fullstack.marketplace.controller;
-
-import no.ntnu.fullstack.marketplace.model.UserInfoResponse;
-import no.ntnu.fullstack.marketplace.dao.AccountDao;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping(value = "/users")
-@EnableAutoConfiguration
-@CrossOrigin
-public class UserInfoController {
-
-    @GetMapping("/{userId}")
-    public UserInfoResponse getUser(@PathVariable("userId") String userId){
-        return new AccountDao().getUser(userId);
-    }
-
-}
+//package no.ntnu.fullstack.marketplace.controller;
+//
+//import no.ntnu.fullstack.marketplace.dao.UserDao;
+//import no.ntnu.fullstack.marketplace.model.UserInfoResponse;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping(value = "/users")
+//@EnableAutoConfiguration
+//@CrossOrigin
+//public class UserInfoController {
+//
+//    @GetMapping("/{userId}")
+//    public UserInfoResponse getUser(@PathVariable("userId") String userId){
+//        return UserDao.getUser(userId);
+//    }
+//
+//}
