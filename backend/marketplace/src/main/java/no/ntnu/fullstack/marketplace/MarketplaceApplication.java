f072885619bfe866f52ad33a30f261d1f7c538d1
Adrian Gunnar Lauterer <adrian@lauterer.it>
2023-03-23 18:09:03 +0100

    fixed some item gui

--- backend/marketplace/src/main/java/no/ntnu/fullstack/marketplace/MarketplaceApplication.java
+++ backend/marketplace/src/main/java/no/ntnu/fullstack/marketplace/MarketplaceApplication.java
@@ -2,6 +2,9 @@ package no.ntnu.fullstack.marketplace;
 
 //import no.ntnu.fullstack.marketplace.repository.UserDataRepository;
 
+import no.ntnu.fullstack.marketplace.model.Item;
+import no.ntnu.fullstack.marketplace.service.ItemService;
+import no.ntnu.fullstack.marketplace.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -17,6 +20,7 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManagerFactory;
 import java.beans.PropertyVetoException;
+import java.util.Arrays;
 
 @Configuration
 @EnableJpaRepositories("no.ntnu.fullstack.marketplace.repository")
@@ -26,15 +30,31 @@ import java.beans.PropertyVetoException;
 public class MarketplaceApplication {
 //    @Autowired
 //    private UserDataRepository userDataRepository;
-
+    @Autowired
+    ItemService itemService;
     public static void main(String[] args) {
         SpringApplication.run(MarketplaceApplication.class, args);
+
+
     }
 
-//    @PostConstruct
-//    private void postInit(){
-//        System.out.println("Users :" + userDataRepository.findAll());
-//    }
+    @PostConstruct
+    private void postInit() {
+        System.out.println("init");
+        System.out.println(itemService.getAllItem());
+        Item item = new Item();
+        item.setName("test");
+        item.setDescription("This is a test item");
+        item.setUserid(1L);
+        item.setPrice(1000L);
+        item.setLocation("New York");
+        item.setImage(Arrays.asList("https://epicmpls.com/wp-content/uploads/2020/03/arozzi_vernazza_bl_vernazza_gaming_chair_blue_1273402.jpg", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.6OHLdGidpuZcpqqqeOk3eAHaHa%26pid%3DApi&f=1&ipt=e971a2f7c54ae6edca4a2f26d56965a915f1359a40e9cb769177edae10d3549b&ipo=images"));
+        item.setCategories(Arrays.asList("category1", "category2"));
+        item.setMetadata(Arrays.asList("metadata1", "metadata2", "metadata3"));
+        itemService.newItem(item);
+        System.out.println(itemService.getAllItem());
+    }
+
 
 
 }
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <LittleItemCard :item="exampleItem"></LittleItemCard>
     <ListOfLittleItems :listOfItems="items"></ListOfLittleItems>
-    <GoogleMap :address="exampleItem.location" />
+    <GoogleMap :address="items[0].location" />
     <RegisterUser />
   </div>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue";
-import LittleItemCard from "@/components/LittleItemCard.vue";
 import ListOfLittleItems from "@/components/ListOfLittleItems.vue";
 import GoogleMap from "@/components/GoogleMap.vue";
 import { getItems } from "@/services/ItemServiceApi";
@@ -20,20 +15,10 @@ export default {
   name: "HomeView",
   components: {
     ListOfLittleItems,
-    HelloWorld,
-    LittleItemCard,
     GoogleMap,
   },
   data() {
     return {
-      exampleItem: {
-        id: 1,
-        name: "Gaming chair for beginner",
-        image:
-          "https://i.pinimg.com/736x/bd/c9/83/bdc9832e5f32ee6168f10536549551bc--kids-bedroom-ideas-girls-bedroom.jpg",
-        location: "Falkenborgvegen 1, 7044 Trondheim",
-        price: 100,
-      },
       items: [],
     };
   },
